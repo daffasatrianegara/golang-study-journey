aed67bedc504d4b677e1031a78a9080832cb9a6a
daffasatrianegara <daffasatria036@gmail.com>
2024-03-13 15:23:01 +0700

    day 1

--- src/1-introduction/name-str.go
+++ src/1-introduction/name-str.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Print("halooo, nama saya daffa, ")
 	fmt.Println("umur saya 21.")
 	
-	// wajib pake printf
+	// wajib pake printf untuk menggunakan variabel/nilai didalam string langsung
 	fmt.Printf("hobi saya adalah %s, %s, dan %s \n", "bermain sepak bola", "membaca buku", "merokok")
 	// %s means to String, if your number are decimal use %d
 	fmt.Printf("saya berumur %d", 21)
