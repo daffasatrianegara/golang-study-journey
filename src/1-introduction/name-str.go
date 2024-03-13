d765504cf837ca81cf3ee86c239b44d9cb5a12d2
daffasatrianegara <daffasatria036@gmail.com>
2024-03-13 15:21:21 +0700

    s=

--- src/1-introduction/name-str.go
+++ src/1-introduction/name-str.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Print("halooo, nama saya daffa, ")
 	fmt.Println("umur saya 21.")
 	
+	// wajib pake printf
 	fmt.Printf("hobi saya adalah %s, %s, dan %s \n", "bermain sepak bola", "membaca buku", "merokok")
 	// %s means to String, if your number are decimal use %d
 	fmt.Printf("saya berumur %d", 21)
